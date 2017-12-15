62ccfe257244ffb00c62aee92304bf6a9c5c74e8
Miguel Díaz <ademouse@gmail.com>
2017-12-15 20:35:34 +0100

    CSS with flexbox first approach

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logos/logo3.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">My discussion blog</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-posts">
+          <h2 className="posts-title">Posts</h2>
+          <header>
+            <h3 class="abstract-title" itemprop="headline"><a href="https://doncoco.es/">Lorem ipsum dolor</a></h3>
+          </header>
+          {/*  ahutor and date here */}
+          <p className="post-intro">
+            First post, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
       </div>
     );
   }
