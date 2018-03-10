20d33cde6b019e1a388c3fc1a77334ebbba8b26c
Miguel Díaz <ademouse@gmail.com>
2018-03-10 19:48:00 +0100

    FetchCategories dispatch only once in app life

--- frontend/src/containers/Category.js
+++ frontend/src/containers/Category.js
@@ -10,29 +10,33 @@ class Category extends React.Component {
   }
   
   componentDidMount(){
-    // TODO temporal: avoid dispatch fetchCategories
-    // this.props.categories.length === 0 &&
-      this.props.dispatch(postActions.fetchCategories());
+    // if not have categories: fetch
+    if(this.props.categories.length === 0){
+      this.props.dispatch(postActions.fetchCategories());  
+    }else{
+      this.conditionsPostsByCategory(this.props);
+    }
   }
   
   componentWillReceiveProps(nextProps){
+    this.conditionsPostsByCategory(nextProps);
+  }
+  
+  conditionsPostsByCategory = (propsToCheck) => {
     // categories received && postsByCategory is empty
-    if(nextProps.categories.length > 0){
+    if(propsToCheck.categories.length > 0){
       const pathSplited = this.props.history.location.pathname.split('/');
       // path with categories
       if(pathSplited.length > 2){
         const categoryFromPath = pathSplited[1];
         // valid category in path
-        if(nextProps.categories.find((cat)=>cat.path === categoryFromPath)){
+        if(propsToCheck.categories.find((cat)=>cat.path === categoryFromPath)){
           this.setState({ headerPostsTitle: categoryFromPath });
           // postsByCategory empty or posts of diferent category 
-          if(nextProps.postsByCategory.length === 0 || categoryFromPath !== nextProps.postsByCategory[0].category){
+          if(propsToCheck.postsByCategory.length === 0 || categoryFromPath !== propsToCheck.postsByCategory[0].category){
             this.props.dispatch(postActions.fetchPostsByCategory(categoryFromPath));
           }
         }
-        // If not fetchPosts or error
-        // change the error control to the fetchCategories action
-        // TODO change error control general only if this matters
       }
     }
   }
